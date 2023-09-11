e34db32a7cb6b00ecca4344f2d78a2a9037fc7e4
Alfan Mubarok <alfan@smolhub.com>
2023-09-12 06:40:03 +0700

    pass: Refactor pass
    In this commit, the following changes were made:
     - Restructure functions and simplify comments
     - Rename variables
     - Add hasSymbol and hasNumber checking in Generate() to ensure only do one checking contains()
     - Refactor contains() to improve readability
     - Simplify isConsecutiveType()

--- pass/pass.go
+++ pass/pass.go
@@ -22,49 +22,18 @@ var (
 	chars   = bytes.Join([][]byte{letters, numbers, symbols}, nil)
 )
 
-// invalidLengthError is the error returned when the password length is invalid.
-var invalidLengthError = errors.New("password length invalid, must be between 1 and 50 characters")
+// invalidLengthError error returns if password length is invalid.
+var invalidLengthError = errors.New("length must be between 1 and 50 characters")
 
-// randByte selects a random byte from a given slice of bytes.
-func randByte(v []byte) byte {
-	l := int64(len(v))
-
-	i, err := rand.Int(rand.Reader, big.NewInt(l))
-	if err != nil {
-		panic(err)
+// contains checks if any byte in subslice 'ss' is present in slice 's'.
+func contains(s, ss []byte) bool {
+	m := make(map[byte]bool, len(s))
+	for _, c := range s {
+		m[c] = true
 	}
 
-	return v[i.Int64()]
-}
-
-// isConsecutiveType checks if two bytes have the same character type.
-func isConsecutiveType(p, n byte) bool {
-	if (unicode.IsLower(rune(p)) && unicode.IsLower(rune(n))) ||
-		(unicode.IsUpper(rune(p)) && unicode.IsUpper(rune(n))) ||
-		(unicode.IsNumber(rune(p)) && unicode.IsNumber(rune(n))) ||
-		(unicode.IsSymbol(rune(p)) && unicode.IsSymbol(rune(n))) {
-		return true
-	}
-	return false
-}
-
-// contains checks if any byte in slice 'v' is present in slice 'p'.
-func contains(p, v []byte) bool {
-	for _, c := range v {
-		for _, val := range p {
-			if val == c {
-				return true
-			}
-		}
-	}
-
-	return false
-}
-
-// isRepeated checks if a byte 'n' is repeated within the given slice 'p'.
-func isRepeated(p []byte, n byte) bool {
-	for _, v := range p {
-		if v == n {
+	for _, c := range ss {
+		if m[c] {
 			return true
 		}
 	}
@@ -72,15 +41,45 @@ func isRepeated(p []byte, n byte) bool {
 	return false
 }
 
-// Generate generates a random string of a specific length. If no length is provided,
-// it returns a default 20-character random string. If an invalid length is provided,
-// it returns an error.
+// isConsecutiveType checks if two bytes have the same character type.
+func isConsecutiveType(p, n byte) bool {
+	var pr, nr = rune(p), rune(n)
+
+	return (unicode.IsLower(pr) && unicode.IsLower(nr)) ||
+		(unicode.IsUpper(pr) && unicode.IsUpper(nr)) ||
+		(unicode.IsNumber(pr) && unicode.IsNumber(nr)) ||
+		(unicode.IsSymbol(pr) && unicode.IsSymbol(nr))
+}
+
+// isRepeated checks if a byte 'b' is repeated within the given slice 's'.
+func isRepeated(s []byte, b byte) bool {
+	for _, v := range s {
+		if v == b {
+			return true
+		}
+	}
+
+	return false
+}
+
+// randByte selects a random byte from a given slice of bytes.
+func randByte(s []byte) byte {
+	l := int64(len(s))
+
+	i, err := rand.Int(rand.Reader, big.NewInt(l))
+	if err != nil {
+		panic(err)
+	}
+
+	return s[i.Int64()]
+}
+
+// Generate generates a random string. If no length is provided, it returns a default
+// 20-character random string. If an invalid length is provided, it returns an error.
 func Generate(n ...int) (string, error) {
-	var l int
+	var l = defaultLength
 	if len(n) > 0 {
 		l = n[0]
-	} else {
-		l = defaultLength
 	}
 
 	if l < minLength || l > maxLength {
@@ -88,25 +87,34 @@ func Generate(n ...int) (string, error) {
 	}
 
 	var p = make([]byte, l)
+	var hasNumber, hasSymbol = false, false
 	for i := range p {
 		for {
-			c := randByte(chars)
+			var c = randByte(chars)
 			if i == 0 {
 				p[i] = c
 				break
 			}
 
-			if !contains(p[:i], symbols) {
-				c = randByte(symbols)
-			}
-
-			if !contains(p[:i], numbers) {
-				c = randByte(numbers)
-			}
-
-			if i != 0 && !isConsecutiveType(p[i-1], c) && !isRepeated(p[:i], c) {
+			if !isConsecutiveType(p[i-1], c) && !isRepeated(p[:i], c) {
 				p[i] = c
 				break
+			}
+
+			if !hasSymbol {
+				if !contains(p[:i], symbols) {
+					p[i] = randByte(symbols)
+					hasSymbol = true
+					break
+				}
+			}
+
+			if !hasNumber {
+				if !contains(p[:i], numbers) {
+					p[i] = randByte(numbers)
+					hasNumber = true
+					break
+				}
 			}
 		}
 	}
